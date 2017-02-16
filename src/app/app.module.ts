c84a4c7643411d4ad6f6d20c822b67a8b3581f9c
Samuel Wang <sam.ajax@gmail.com>
2017-02-16 00:43:37 -0500

    create root component placeholder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DesignerComponent } from './designer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DesignerComponent
   ],
   imports: [
     BrowserModule,
