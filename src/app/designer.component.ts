604c1a1d2e5f4a56aa83b7c51e7e8c9842732eae
Samuel Wang <sam.ajax@gmail.com>
2017-02-17 03:44:40 -0500

    port metrojs core into the designer app

--- src/app/designer.component.ts
+++ src/app/designer.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, ViewEncapsulation } from '@angular/core';
+var Metro = require("../vendor/metrojs/metro").Metro;
 
 @Component({
   selector: 'metro-designer',
   templateUrl: './designer.component.html',
-  styleUrls: ['./designer.component.css']
+  styleUrls: ['./designer.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
+
 export class DesignerComponent {
   title = 'Metro Designer';
-  time = {hour: 13, minute: 30};
+  app = {
+    canvasWidth: <number> null,
+    canvasHeight: <number> null
+  };
+
+  @ViewChild('container') container:ElementRef;
+
+  ngAfterViewInit() {
+
+    let def = {
+      pointerRadius: 10,
+      width: 800,
+      height: 500,
+      resolution: 20,
+      container: this.container.nativeElement,
+      inputMode: 'draw',
+      pathType: 'straight',
+    };
+    console.log(def);
+
+    let metro = new Metro(def);
+
+    this.app.canvasWidth = metro.width;
+    this.app.canvasHeight = metro.height
+
+  }
 }
